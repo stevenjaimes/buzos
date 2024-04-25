391ac62b3a8e8aba6e1f74844d03450c2bfed70a
Steven <hensteve250@gmail.com>
2024-04-25 13:35:06 -0500

    act final

--- src/app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -52,6 +52,7 @@ const formSchema = z.object({
   name: z.string().min(1),
   images: z.object({ url: z.string() }).array(),
   price: z.coerce.number().min(1),
+ 
   categoryId: z.string().min(1),
   colorId: z.string().min(1),
   sizeId: z.string().min(1),
