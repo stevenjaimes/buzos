a465062587b660eb5a60bb20a8e891ae3a0719ba
Steven <hensteve250@gmail.com>
2024-04-25 01:55:16 -0500

    denuevo

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/:path*","/dashboard"],
+  publicRoutes: ["/", "/api/:path*"],
 });
 
 export const config = {
