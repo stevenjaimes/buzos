ab87f9cdabece3af209a5df4def13a634b8fe0fc
Steven <hensteve250@gmail.com>
2024-04-25 13:35:46 -0500

    nuevaact

--- src/components/ui/api-list.tsx
+++ src/components/ui/api-list.tsx
@@ -13,7 +13,7 @@ export default function ApiList({ entityName, entityIdName }: ApiListProps) {
   const params = useParams();
   const origin = useOrigin();
 
-  const baseUrl = `${origin}/admin/api/${params.storeId}`;
+  const baseUrl = `${origin}/api/${params.storeId}`;
 
   return (
     <>
