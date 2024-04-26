9efe09fd74caf5d6a2d25fdc62063e2520111fd6
Steven <hensteve250@gmail.com>
2024-04-26 12:32:18 -0500

    actualizaciones

--- src/app/(routes)/admin/(dashboard)/[storeId]/layout.tsx
+++ src/app/(routes)/admin/(dashboard)/[storeId]/layout.tsx
@@ -25,7 +25,7 @@ export default async function DashboardLayout({
   })
 
   if (!store) {
-    redirect("/admin/sign-in");
+    redirect("/sign-in");
   }
 
   return (
--- src/app/(routes)/admin/(root)/layout.tsx
+++ src/app/(routes)/admin/(root)/layout.tsx
@@ -21,7 +21,7 @@ export default async function SetUpLayout({
   });
 
   if (store) {
-    redirect(`/admin/${store.id}`);
+    redirect(`/${store.id}`);
   };
 
   return (
